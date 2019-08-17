316c0f781bb683eebf70a7f8d7b4d5f5d6922cff
bashkos <bashkos@gmail.com>
2019-08-17 22:03:13 +0900

    Fix home page styles

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { history } from '../../store/configureStore';
 
+const useStyles = makeStyles(theme => ({
+  home: {
+    padding: 20
+  }
+}));
+
 export default function Home() {
+  const classes = useStyles();
+
   return (
-    <Grid container justify="center" alignItems="center" direction="column">
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      direction="column"
+      className={classes.home}
+    >
       <Grid item>
         <Grid container justify="center" spacing="5">
           <Grid item>
